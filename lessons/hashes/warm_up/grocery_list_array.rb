4b97ada2befb69539811b14920840f951f5fcb01
Antoine Aube <aaube3@gmail.com>
2023-07-17 09:03:23 -0600

    solutions to hashes exercises

--- lessons/hashes/warm_up/grocery_list_array.rb
+++ lessons/hashes/warm_up/grocery_list_array.rb
@@ -8,4 +8,14 @@ fridge_items_1 = ["milk", "eggs", "eggs","eggs", "eggs",
 # 1. What is problematic about listing `fridge_items_1`
 # as an array structured grocery list?
 
+#unable to easily search for quantities or items
+#redundanty in general
+
 # 2. What might be a more readable way to structure this?
+
+groceries = {
+  "milk" => 0, 
+  "eggs" => 0,
+  "avocado" => 0,
+  "tortilla" => 0
+}
--- lessons/hashes/warm_up/suitcase_hash.rb
+++ lessons/hashes/warm_up/suitcase_hash.rb
@@ -2,7 +2,9 @@ suitcase = {
   "socks" => 4,
   "pants" => 1,
 }
-
+# require 'pry';binding.pry
 # 1. Add a key/value pair of shirts/3 to the suitcase.
+
+suitcase[:shirts] = 3
 # 2. Increase the value of shirts by 1.
 # 3. Add a key/value pair of swimsuit/true to the suitcase.
