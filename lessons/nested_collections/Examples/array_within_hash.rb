55669f832647253a60002d83f2c25c576a3327b8
Antoine Aube <aaube3@gmail.com>
2023-07-18 10:06:56 -0600

    small edit

--- lessons/nested_collections/Examples/array_within_hash.rb
+++ lessons/nested_collections/Examples/array_within_hash.rb
@@ -11,8 +11,10 @@ puts count
 values = pizza_toppings.values
 p values
 # 3. how can I access the element “pineapple”
-pineapple = pizza_toppings[:fruit]
+pineapple = pizza_toppings[:fruit][0]
 p pineapple
 # 4. how can I add the element “olives” to the key “veggies”?
 add_olives = pizza_toppings[:veggies].prepend("olives")
+#can alsoo do pizza_toppings[:veggies] += ["olives"] -> when using += have to give
+#the data type aka []
 p add_olives
