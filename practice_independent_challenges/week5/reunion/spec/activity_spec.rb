7728cd0819c8ac8975a3a8c003741c3620247a2a
Antoine Aube <aaube3@gmail.com>
2023-08-07 19:15:30 -0600

    test: #owed

--- practice_independent_challenges/week5/reunion/spec/activity_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/activity_spec.rb
@@ -35,7 +35,16 @@ RSpec.describe Activity do
       activity.add_participant("Luther", 40)
 
       expect(activity.total_cost).to eq(60)
-      expect(activity,split).to eq(30)
+      expect(activity.split).to eq(30)
+    end
+  end
+
+  describe "owed" do 
+    it "can deduct split from amount participant paint" do 
+      activity.add_participant("Maria", 20)
+      activity.add_participant("Luther", 40)
+
+      expect(activity.owed).to eq({"Maria" => 10, "Luther" => -10})
     end
   end
 end
