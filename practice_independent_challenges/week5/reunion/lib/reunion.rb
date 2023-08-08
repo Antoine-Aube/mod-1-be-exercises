ce60d53540bae39f336e5b37f8112509c975ab6f
Antoine Aube <aaube3@gmail.com>
2023-08-07 20:14:43 -0600

    feat: #total_owed

--- practice_independent_challenges/week5/reunion/lib/reunion.rb
+++ practice_independent_challenges/week5/reunion/lib/reunion.rb
@@ -9,4 +9,18 @@ class Reunion
   def add_activity(activity)
     @activities << activity
   end
+
+    def reunion_cost
+      @activities.map do |activity|
+        activity.total_cost 
+      end.sum
+    end
+
+    def total_owed
+     @activities.each_with_object(Hash.new(0)) do |activity, owed_totals|
+        activity.owed.each do |participant, owed_amount|
+          owed_totals[participant] += owed_amount
+        end
+      end
+    end
 end
--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -4,6 +4,7 @@ require './lib/activity'
 RSpec.describe Reunion do 
   let(:reunion) {Reunion.new("1406 BE")}
   let(:activity_1) {Activity.new("Brunch")}
+  let(:activity_2) {Activity.new("Drinks")}
 
   describe "Initialize" do 
    it{expect(reunion).to be_a Reunion}
@@ -17,6 +18,35 @@ RSpec.describe Reunion do
       reunion.add_activity(activity_1)
       
       expect(reunion.activities).to eq([activity_1])
+    end
+  end
+
+  describe "#reunion_cost" do
+    it "can return the total cost of the reunion summing the costs of the activities" do 
+      activity_1.add_participant("Maria", 20)
+      activity_1.add_participant("Luther", 40)
+      activity_2.add_participant("Maria", 40)
+      activity_2.add_participant("Luther", 0)
+
+      reunion.add_activity(activity_1)
+      reunion.add_activity(activity_2)
+
+      expect(reunion.reunion_cost).to eq(100)
+    end
+
+    it "can return the total cost of the reunion summing the costs of the activities" do 
+      activity_1.add_participant("Maria", 20)
+      activity_1.add_participant("Luther", 40)
+      activity_2.add_participant("Maria", 40)
+      activity_2.add_participant("Luther", 0)
+
+      reunion.add_activity(activity_1)
+      reunion.add_activity(activity_2)
+
+      expect(activity_1.owed).to eq({"Maria" => 10, "Luther" => -10})
+      expect(activity_2.owed).to eq({"Maria" => -20, "Luther" => 20})
+
+      expect(reunion.total_owed).to eq({"Maria" => -10, "Luther" => 10})
     end
   end
 end
