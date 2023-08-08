392debd7a9b49d8af9d6c920543ff8a4e1634d3b
Antoine Aube <aaube3@gmail.com>
2023-08-07 19:35:15 -0600

    feat: #add_activity

--- practice_independent_challenges/week5/reunion/lib/reunion.rb
+++ practice_independent_challenges/week5/reunion/lib/reunion.rb
@@ -1,7 +1,12 @@
 class Reunion
+  attr_reader :name, :activities
 
-  attr_reader :name
   def initialize(name)
     @name = name
+    @activities = []
+  end
+
+  def add_activity(activity)
+    @activities << activity
   end
 end
--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -1,11 +1,22 @@
 require './lib/reunion'
+require './lib/activity'
 
 RSpec.describe Reunion do 
   let(:reunion) {Reunion.new("1406 BE")}
-  let(:activity) {Activity.new("Brunch")}
+  let(:activity_1) {Activity.new("Brunch")}
 
   describe "Initialize" do 
    it{expect(reunion).to be_a Reunion}
    it{expect(reunion.name).to eq("1406 BE")}
+  end
+
+  describe "add_activity" do 
+    it "can add activities to its array" do 
+      expect(reunion.activities).to eq([])
+
+      reunion.add_activity(activity_1)
+      
+      expect(reunion.activities).to eq([activity_1])
+    end
   end
 end
