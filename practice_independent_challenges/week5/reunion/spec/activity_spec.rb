21daff6bef4e4d072465be0205703c8cedbb1e3e
Antoine Aube <aaube3@gmail.com>
2023-08-07 19:05:42 -0600

    test: #add_participant, #total_cost

--- practice_independent_challenges/week5/reunion/spec/activity_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/activity_spec.rb
@@ -7,6 +7,25 @@ RSpec.describe Activity do
     it "exists and has a name" do 
       expect(activity).to be_a Activity
       expect(activity.name).to eq("Brunch")
+      expect(activity.participants).to eq({})
     end
+  end
+
+  describe "#add_participants" do 
+    it "can add participants to participants hash" do 
+      activity.add_participant("Maria", 20)
+
+      expect(activity.participants).to eq({"Maria" => 20})
+    end
+  end
+
+  describe "total_cost" do 
+    it "can calculate total cost" do 
+      activity.add_participant("Maria", 20)
+      activity.add_participant("Luther", 40)
+      require 'pry';binding.pry
+      expect(activity.participants).to eq({"Maria" => 20, "Luther" => 40})
+      expect(activity.total_cost).to eq(60)
+    end 
   end
 end
