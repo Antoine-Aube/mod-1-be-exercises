7d5ac06f4272c7497d210ab84d93572a224587ea
Antoine Aube <aaube3@gmail.com>
2024-02-18 11:50:32 -0700

    methods for venue class practice IC

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,27 @@
 class Venue
+  attr_reader :capacity, :name, :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+    def add_patron(patron)  
+      @patrons << patron
+    end
+
+    def yell_at_patrons
+      @patrons.map do | patron |
+        patron.upcase
+      end
+    end
+
+    def over_capacity?
+      @patrons.length > @capacity
+    end
+
+    def kick_out
+      @patrons.pop 
+    end
 end
