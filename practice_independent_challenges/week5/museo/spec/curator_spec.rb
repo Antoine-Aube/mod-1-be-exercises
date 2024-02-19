dd75f67ed1ea9f36ef07fdfc2aab1cee9f145ccc
Antoine Aube <aaube3@gmail.com>
2024-02-18 20:14:53 -0700

    tests: curator initialize and add photo and artists methods

---
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -0,0 +1,62 @@
+require './lib/photograph'
+require './lib/curator'
+require './lib/artist'
+
+RSpec.describe Curator do 
+  let (:curator) {Curator.new}
+  let (:photo_1) { Photograph.new({
+        id: "1",      
+        name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+        artist_id: "1",      
+        year: "1954"      
+    })
+  }
+
+let (:photo_2) { Photograph.new({
+      id: "2",      
+      name: "Moonrise, Hernandez",      
+      artist_id: "2",      
+      year: "1941"      
+    }) 
+  }
+
+let (:artist_1) { Artist.new({
+      id: "1",      
+      name: "Henri Cartier-Bresson",      
+      born: "1908",      
+      died: "2004",      
+      country: "France"      
+    }) 
+  }
+
+let (:artist_2) {Artist.new({
+      id: "2",      
+      name: "Ansel Adams",      
+      born: "1902",      
+      died: "1984",      
+      country: "United States"      
+    })
+  }
+
+  describe "iniatilize" do 
+    it {expect(curator).to be_a(Curator)}
+    it {expect(curator.artists).to eq([])}
+    it {expect(curator.photographs).to eq([])}
+  end
+
+  describe "curator class instance methods" do 
+    it "adds photographs and artists" do 
+      expect(curator.artists).to eq([])
+      expect(curator.photographs).to eq([])
+      
+      curator.add_artist(artist_1)
+      curator.add_artist(artist_2)
+      curator.add_photograph(photo_1)
+      curator.add_photograph(photo_2)
+      
+      expect(curator.artists).to eq([artist_1, artist_2])
+      expect(curator.photographs).to eq([photo_1, photo_2])
+    end
+  end
+  
+end
