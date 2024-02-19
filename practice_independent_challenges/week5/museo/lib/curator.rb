82d26fc7ac45f57dad767224355e1d4b9e782a79
Antoine Aube <aaube3@gmail.com>
2024-02-18 20:14:24 -0700

    curator class with add photo and artist methods

---
+++ practice_independent_challenges/week5/museo/lib/curator.rb
@@ -0,0 +1,17 @@
+class Curator
+  attr_reader :artists, :photographs 
+
+  def initialize
+    @artists = []
+    @photographs = []
+  end
+
+  def add_artist(artist)
+    @artists << artist
+  end
+
+  def add_photograph(photograph)
+    @photographs << photograph
+  end
+
+end
